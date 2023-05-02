1f22cb94c8df3f47664396ed6438e213f85c6154
Daniel Costa <daniel.costa@syngenta.com>
2023-05-02 20:05:26 -0300

    Added validations

--- src/main/kotlin/com/daniel/forum/controller/TopicoController.kt
+++ src/main/kotlin/com/daniel/forum/controller/TopicoController.kt
@@ -3,6 +3,7 @@ package com.daniel.forum.controller
 import com.daniel.forum.dto.NovoTopicoDto
 import com.daniel.forum.model.Topico
 import com.daniel.forum.service.TopicoService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -23,7 +24,7 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody dto: NovoTopicoDto) {
+    fun cadastrar(@RequestBody @Valid dto: NovoTopicoDto) {
         service.cadastrar(dto)
     }
 }
--- src/main/kotlin/com/daniel/forum/dto/NovoTopicoDto.kt
+++ src/main/kotlin/com/daniel/forum/dto/NovoTopicoDto.kt
@@ -1,7 +1,12 @@
 package com.daniel.forum.dto
 
+import jakarta.validation.constraints.NotEmpty
+
+
 data class NovoTopicoDto(
+    @field:NotEmpty
     val titulo: String,
+    @field:NotEmpty
     val mensagem: String,
     val idCurso: Long,
     val idAutor: Long
